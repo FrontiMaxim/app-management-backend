1ea825e825168de2abb426bfc98055de3ba9d32e
Максим Расторгуев <frontimax@yandex.ru>
2023-03-27 11:11:12 +0400

    fixed role check

--- src/helpers/checkRole.ts
+++ src/helpers/checkRole.ts
@@ -2,7 +2,7 @@ import { Response } from "express";
 import { RoleError } from "../errors/role.error";
 
 export const checkRole = (allowedRoles: string[], role: string, res: Response) => {
-    if(allowedRoles.includes(role)) {
+    if(!allowedRoles.includes(role)) {
         res.status(403).send(new RoleError().message);
     }
 }
